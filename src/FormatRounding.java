2cadf7db467d017b5653c5783450c653c7f443b4
Hui Xue <82717571+IAmHuiXue@users.noreply.github.com>
2022-03-05 13:39:50 -0500

    refactored

--- src/FormatRounding.java
+++ src/FormatRounding.java
@@ -3,8 +3,11 @@ import java.text.DecimalFormat;
 public class FormatRounding {
     public static void main(String[] args) {
         DecimalFormat formatter = new DecimalFormat("#0.00");
+        // .format() -> returns a string
+        String res = formatter.format(1.1);
+        Float resFloat = Float.valueOf(res);
+        System.out.println(formatter.format(resFloat));
 
-        System.out.println(formatter.format(1.1));
         System.out.println(formatter.format(1.11));
         System.out.println(Float.valueOf(formatter.format(1.121)));
     }
